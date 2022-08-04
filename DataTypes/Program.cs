66daa625d29a4555f96ff0ff8355bb83dbd94a91
Stoyan06 <stoyanzlankov_2006@abv.bg>
2022-08-04 06:38:19 +0300

    Added 11 ex.
    ex.11 overloads dont work!

---
+++ DataTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if(type == "int")
+            {
+                int a = int.Parse(Console.ReadLine());
+                Print(a);
+            }
+
+            if (type == "double")
+            {
+                double b = double.Parse(Console.ReadLine());
+                Print(b);
+            }
+
+            if (type == "string")
+            {
+                string c = Console.ReadLine();
+                Print(c);
+            }
+
+            static void Print(int input)
+            {
+                Console.WriteLine(input * 2);
+            }
+
+            static void Print(double input2)
+            {
+                Console.WriteLine("{0:f2}",input2 * 1.5);
+            }
+
+            static void Print(string input3)
+            {
+                Console.WriteLine("$" + input3 + "$");
+            }
+        }
+    }
+}
